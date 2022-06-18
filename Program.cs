ea99d29b799d44695497c65d90377cd329a31c98
Роман <ram23@mail.ru>
2022-06-18 18:03:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_12_1_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            List<User> listuser = new List<User>
+            {
+                new User() { Name = "Igor", Login = 2345, IsPremium = true  },
+
+                new User() { Name = "Roman", Login = 2342, IsPremium = false  },
+
+                new User() { Name = "Natasha", Login = 6798, IsPremium = true}
+            };
+
+            foreach (var item in listuser)
+            {
+                if (item.IsPremium == false)
+                {
+                    User.ShowAds();
+                }
+                else Console.WriteLine($"Hello {item.Name}");
+            }
+
+           
+        }
+    }
+}
